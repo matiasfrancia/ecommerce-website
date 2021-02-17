6684ee8c94339b2407176713c61b0e5375767ae7
Yoshiro1412 <josiasrojasramires@gmail.com>
2021-02-16 21:49:41 -0300

    Add contact form

--- ecommerce/contact/views.py
+++ ecommerce/contact/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from .forms import EmailForm
 
 # Create your views here.
 
 def contact(request):
 
+    form = EmailForm(request.POST or None)
+
     if request.method == "POST":
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        email = request.POST.get('email')
         
-        if email and message:
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            email = form.cleaned_data['email']
+            
             print(subject,message,email)
             send_mail(
                 subject,
-                message,
-                email,
-                [''],
+                message+"\nFrom: "+email,
+                '',
+                [''], #Correo de la empresa
                 fail_silently=False,
             )
 
 
-    return render(request, "contact.html")
+    return render(request, "contact.html", {"form": form})
 
 
