5a461ad802d470df75a252266f4e3c16f6c6cae2
matiasfrancia <matiasfrancia1@gmail.com>
2021-03-19 18:34:13 -0300

    static files agregados

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,6 +22,9 @@ from django.conf.urls.static import static
 from . import views
 from register import views as register_views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
@@ -40,3 +43,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
